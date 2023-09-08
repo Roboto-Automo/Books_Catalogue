19e0e7dcb522ccbb29dbcb01dd295b859ad59abf
Automoroboto <roberto@studiodespirito.co.uk>
2023-09-08 20:25:57 +0100

    initial commit book scaffold and auth devise

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+b1 = Book.create(title: "The Hobbit", author: "J.R.R. Tolkien", rating: 5)
+b2 = Book.create(title: "The Fellowship of the Ring", author: "J.R.R. Tolkien", rating: 5)
+b3 = Book.create(title: "The Two Towers", author: "J.R.R. Tolkien", rating: 5)
+b4 = Book.create(title: "The Return of the King", author: "J.R.R. Tolkien", rating: 5)
+b5 = Book.create(title: "The Silmarillion", author: "J.R.R. Tolkien", rating: 5)
+b6 = Book.create(title: "The Children of Hurin", author: "J.R.R. Tolkien", rating: 5)
+b7 = Book.create(title: "The Fall of Gondolin", author: "J.R.R. Tolkien", rating: 5)
+b8 = Book.create(title: "The Legend of Sigurd and Gudrun", author: "J.R.R. Tolkien", rating: 5)
