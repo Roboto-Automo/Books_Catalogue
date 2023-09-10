ce3e6f4766d5dcde69413b3d6bcdedbe31f99208
Automoroboto <roberto@studiodespirito.co.uk>
2023-09-10 17:52:10 +0100

    more page styling and authentication

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,14 @@ class Book < ApplicationRecord
     def self.ransackable_attributes(auth_object = nil)
         ["author", "rating", "title"]
       end
+
+    def self.ransackable_associations(auth_object = nil)
+        []
+    end
+
+    validates :title, presence: true
+    validates :author, presence: true
+    validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+
+
 end
