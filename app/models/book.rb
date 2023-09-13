0af4962cf82314e05be1f3350435034e2fb42520
Automoroboto <roberto@studiodespirito.co.uk>
2023-09-13 18:57:46 +0100

    change validation to allow same title across user accounts

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,8 +8,7 @@ class Book < ApplicationRecord
         []
     end
     validates :user_id, presence: true
-    validates :title, presence: true
-    validates :title, uniqueness: true
+    validates :title, presence: true, uniqueness: { scope: :user_id }
     validates :author, presence: true
     validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 
